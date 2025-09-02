4a2ee527ceda099019bb9c7e002fc249a6dc16b5
Thiago.Mausbach <thiagomausbach@gmail.com>
2025-09-02 17:48:39 -0300

    DB integration

---
+++ SecondAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecondAPI.Model;
+
+namespace SecondAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Dados> Livros { get; set; }
+}
