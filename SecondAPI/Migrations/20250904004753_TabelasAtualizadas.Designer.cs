70e66b01eb8c83932b35fcdc42eeb30874354d3d
Thiago.Mausbach <thiagomausbach@gmail.com>
2025-09-03 21:59:18 -0300

    DbSet Users e Model LivroEmprestado

--- SecondAPI/Migrations/20250904004753_TabelasAtualizadas.Designer.cs
+++ SecondAPI/Migrations/20250904004753_TabelasAtualizadas.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecondAPI.Context;
 
@@ -10,9 +11,11 @@ using SecondAPI.Context;
 namespace SecondAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250904004753_TabelasAtualizadas")]
+    partial class TabelasAtualizadas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
